3ad848a754b98ec170ba99b3857abfd5a7ce6f37
julitacreations <90911265+julitacreations@users.noreply.github.com>
2023-12-04 18:30:58 +0100

    Mobile menu button completed
    still need to close menu once clicking on link

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 $( document ).ready(function() {
     
     $(".burger-button").click(function(){
-     
+      
         $(".burger-button").toggleClass("active");
         $(".mobile-menu").toggleClass("active");
         

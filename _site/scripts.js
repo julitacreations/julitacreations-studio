de382cd4c9505cb174ae4a18db3c4ba9f841aa7b
julitacreations <90911265+julitacreations@users.noreply.github.com>
2023-12-04 16:41:07 +0100

    working on mobile menu button

---
+++ _site/scripts.js
@@ -0,0 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $(".burger-button").click(function(){
+     
+        $(".burger-button").toggleClass("active");
+        $(".mobile-menu").toggleClass("active");
+        
+      });
+
+
+
+});

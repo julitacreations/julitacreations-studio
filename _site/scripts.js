254a089275ccfed23fda5425f34ef8e933ae622f
julitacreations <90911265+julitacreations@users.noreply.github.com>
2023-12-21 15:23:56 +0100

    JS added for accordion Services section

--- _site/scripts.js
+++ _site/scripts.js
@@ -10,11 +10,33 @@ $( document ).ready(function() {
       });
 
 
+    // Lightgallery Pop-up
+    lightGallery(document.getElementById('lightgallery'), {
+      
+      speed: 500,
+      download: false
+      
+    });
+
+    // Accordion
+    $('.accordion-header').click(function(){
+      // Toggle the visibility of the content when the header is clicked
+      $(this).next('.accordion-content').slideToggle();
+      // Toggle active class to highlight the active section
+      $(this).toggleClass('active');
+      // Toggle the + and - signs
+      $(this).find('::after').html($(this).hasClass('active') ? '-' : '+');
+      // Hide other content when a new section is opened
+      $('.accordion-content').not($(this).next('.accordion-content')).slideUp();
+      // Remove active class from other headers
+      $('.accordion-header').not(this).removeClass('active');
+      // Reset the + and - signs on other headers
+      $('.accordion-header').not(this).find('::after').html('+');
+    });
+
+
 });
 
- // Lightgallery Pop-up
- lightGallery(document.getElementById('lightgallery'), {
-  speed: 500,
-  download: false
-  
-});
+
+
+

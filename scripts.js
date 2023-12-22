7e1b99a83f8feeb7e3fe736edd8ec98509d5b46f
julitacreations <90911265+julitacreations@users.noreply.github.com>
2023-12-22 17:43:22 +0100

    responsive classes started for footer and main banner section

--- scripts.js
+++ scripts.js
@@ -11,7 +11,14 @@ $( document ).ready(function() {
 
 
     // Magnific Popup Gallery
-      $('.image-link').magnificPopup({type:'image'});
+
+    // This will create a single gallery from all elements that have class "gallery-item"
+    $('.gallery-item').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled:true
+      }
+    });
     
 
     // Accordion

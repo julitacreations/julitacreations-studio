264b6ec3d49350981ad8feaebf399c27c65bfe35
julitacreations <90911265+julitacreations@users.noreply.github.com>
2024-01-05 15:56:18 +0100

    arrows slick slider added

--- _site/slick.js
+++ _site/slick.js
@@ -12,7 +12,7 @@ if (typeof jQuery === 'undefined') {
         autoplay: true, // Do we want it to autoplay? true or false
         autoplaySpeed: 3000, // How long between each slide when auto-playing
         speed: 800, // How fast is the transition in milliseconds
-        arrows: false, // Do you want to show arrows to trigger each slide
+        arrows: true, // Do you want to show arrows to trigger each slide
         accessibility: true, // Enables keyboard tabbing and arrow key navigation
         dots: true, // Enables the dots below to show how many slides
         fade: false, // Changes the animate from slide to fade if true

e1ffb3fd90a90b40f85c191aeaf655ad8a12f51b
julitacreations <90911265+julitacreations@users.noreply.github.com>
2025-11-18 17:15:06 +0100

    removed custom cursor for now- seems slow- might be cloudflare outage

--- scripts.js
+++ scripts.js
@@ -271,22 +271,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 //custom cursor
-const cursor = document.getElementById('custom-cursor');
+// const cursor = document.getElementById('custom-cursor');
 
-document.addEventListener('mousemove', e => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
-});
+// document.addEventListener('mousemove', e => {
+//   cursor.style.left = e.clientX + 'px';
+//   cursor.style.top = e.clientY + 'px';
+// });
 
-// Enlarge and make transparent on hover
-document.querySelectorAll('a, button').forEach(el => {
-  el.addEventListener('mouseenter', () => {
-    cursor.style.transform = 'translate(-50%, -50%) scale(2)';
-    cursor.style.opacity = '0.5'; // semi-transparent
-  });
-  el.addEventListener('mouseleave', () => {
-    cursor.style.transform = 'translate(-50%, -50%) scale(1)';
-    cursor.style.opacity = '1'; // fully visible
-  });
-});
+// // Enlarge and make transparent on hover
+// document.querySelectorAll('a, button').forEach(el => {
+//   el.addEventListener('mouseenter', () => {
+//     cursor.style.transform = 'translate(-50%, -50%) scale(2)';
+//     cursor.style.opacity = '0.5'; // semi-transparent
+//   });
+//   el.addEventListener('mouseleave', () => {
+//     cursor.style.transform = 'translate(-50%, -50%) scale(1)';
+//     cursor.style.opacity = '1'; // fully visible
+//   });
+// });
 
